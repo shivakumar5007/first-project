d7a6155285588c2db787967b0f133a48dae377db
shivakumar <changomula.shivakumar@gmail.com>
2024-04-24 06:17:57 +0000

    second commit

--- firstjava.java
+++ firstjava.java
@@ -1,7 +1,6 @@
-class firstproject
-{
-	public static void main( String args[])
+class first{
+	public static void main(String args[])
 	{
-		System.out.println("hi this is my first devops project- building ci-cd pipeline usinf jenkins ansible and all");
+		System.out.println("hello");
 	}
 }
